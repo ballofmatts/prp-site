1f5e4950a7cf8f7e3fb5f0c249726d7ac4478f11
Matt <ballofmatts@gmail.com>
2022-05-31 20:29:03 -0400

    Adding queryparams and links from seasons to episodes

--- src/app/episode-blurbs/episode-blurbs.component.ts
+++ src/app/episode-blurbs/episode-blurbs.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {PaginationInstance} from 'ngx-pagination';
 import {Podcast, PodcastItem} from '@models';
 import {PodcastInfoService} from '@services';
+import {ActivatedRoute} from '@angular/router';
+import {switchMap} from 'rxjs/operators';
 
 const EPISODES_PER_PAGE = 10;
 
@@ -30,15 +32,50 @@ export class EpisodeBlurbsComponent implements OnInit {
 
   constructor(
     private podcastInfoService: PodcastInfoService,
+    private route: ActivatedRoute,
   ) {
   }
 
   ngOnInit() {
-    // init the podcast data
-    this.podcastInfoService.podcastData.subscribe((podcast: Podcast) => {
-      this.podcastData = podcast;
-      this.onFiltersChanged();
-    });
+    this.podcastInfoService.podcastData
+      .pipe(
+        switchMap((podcast: Podcast) => {
+          // init the podcast data
+          this.podcastData = podcast;
+          return this.route.queryParams;
+        })
+      )
+      .subscribe(params => {
+        // then handle any query params
+        if (!!params.show) {
+          switch (params.show) {
+            case 'party-roll':
+              this.showTypeFilter = 'Party Roll';
+              break;
+            case 'savage-roll':
+              this.showTypeFilter = 'Savage Roll';
+              break;
+            case 'snack-roll':
+              this.showTypeFilter = 'Snack Roll';
+              break;
+          }
+        }
+        this.onShowChange();
+        if (!!params.season && !isNaN(params.season) && this.availableSeasons.includes(parseInt(params.season, 10))) {
+          this.seasonFilter = params.season;
+        }
+        if (!!params.order) {
+          switch (params.order) {
+            case '-1':
+              this.sortOrder = '-pubDate';
+              break;
+            case '1':
+              this.sortOrder = 'pubDate';
+              break;
+          }
+        }
+        this.onFiltersChanged();
+      });
   }
 
   onPageChange(page: number) {
@@ -66,17 +103,19 @@ export class EpisodeBlurbsComponent implements OnInit {
     }
   }
 
-  ohShowChange() {
-    // if they picked an individual show, get the available seasons based on actual podcast data
-    if (this.showTypeFilter !== 'all') {
-      this.availableSeasons = this.podcastData.items
-        .filter(item => item.showType === this.showTypeFilter)
-        .map(item => item.season);
-      this.availableSeasons = [...new Set(this.availableSeasons)].sort();
-    } else { // otherwise, clear em out
-      this.availableSeasons = [];
+  onShowChange() {
+    if (!!this.podcastData) {
+      // if they picked an individual show, get the available seasons based on actual podcast data
+      if (this.showTypeFilter !== 'all') {
+        this.availableSeasons = this.podcastData.items
+          .filter(item => item.showType === this.showTypeFilter)
+          .map(item => item.season);
+        this.availableSeasons = [...new Set(this.availableSeasons)].sort();
+      } else { // otherwise, clear em out
+        this.availableSeasons = [];
+      }
+      this.seasonFilter = 'all';
+      this.onFiltersChanged();
     }
-    this.seasonFilter = 'all';
-    this.onFiltersChanged();
   }
 }
